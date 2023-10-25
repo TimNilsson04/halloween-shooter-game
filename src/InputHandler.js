bd6f31d2a9471037dfce2c8c7dee25d2620dc8a2
timnilsson04 <timnils04@gmail.com>
2023-10-25 12:53:45 +0200

    ✨

--- src/InputHandler.js
+++ src/InputHandler.js
@@ -15,7 +15,7 @@ export default class InputHandler {
         this.game.keys.indexOf(event.key) === -1
       ) {
         this.game.keys.push(event.key)
-
+        this.game.player.shoot()
       }
 
       // if (event.key === ' ') {
