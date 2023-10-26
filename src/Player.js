0068fbeec32ca89bd5e16a5a89138a1367b951b6
timnilsson04 <timnils04@gmail.com>
2023-10-26 09:09:20 +0200

    ✨

--- src/Player.js
+++ src/Player.js
@@ -78,19 +78,19 @@ export default class Player {
       projectile.draw(context)
     })
 
-    // if (this.game.debug) {
-    //   context.strokeRect(this.x, this.y, this.width, this.height)
-    //   context.fillStyle = 'black'
-    //   context.font = '12px Arial'
-    //   context.fillText(this.frameX, this.x, this.y - 5)
-    //   context.fillText(this.grounded, this.x + 20, this.y - 5)
+    if (this.game.debug) {
+      context.strokeRect(this.x, this.y, this.width, this.height)
+      context.fillStyle = 'black'
+      context.font = '12px Arial'
+      context.fillText(this.frameX, this.x, this.y - 5)
+      context.fillText(this.grounded, this.x + 20, this.y - 5)
 
-    //   const x = this.direction === 1 ? this.x + this.width + 10 : this.x - 10
-    //   context.beginPath()
-    //   context.moveTo(this.x + this.width / 2, this.y + this.height / 2)
-    //   context.lineTo(x, this.y + this.height / 2)
-    //   context.stroke()
-    // }
+      const x = this.direction === 1 ? this.x + this.width + 10 : this.x - 10
+      context.beginPath()
+      context.moveTo(this.x + this.width / 2, this.y + this.height / 2)
+      context.lineTo(x, this.y + this.height / 2)
+      context.stroke()
+    }
   }
 
 
