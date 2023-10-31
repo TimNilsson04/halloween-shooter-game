ea526b0060329201d970c27bb6d8d6ec6363e45b
timnilsson04 <timnils04@gmail.com>
2023-10-31 13:42:21 +0100

    ✨

---
+++ src/Background.js
@@ -0,0 +1,22 @@
+import backgroundImage from './assets/layers/backgroundImage.jpg'
+import Layer from './Layer'
+
+export default class Background {
+  constructor(game) {
+    this.game = game
+    const background = new Image()
+    background.src = backgroundImage
+    this.backgroundLayer = new Layer(this.game, background, 1708, 500, 0.2)
+    this.layers = [
+      this.backgroundLayer
+    ]
+  }
+
+  update() {
+    this.layers.forEach((layer) => layer.update())
+  }
+
+  draw(context) {
+    this.layers.forEach((layer) => layer.draw(context))
+  }
+}
